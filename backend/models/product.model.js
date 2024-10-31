efd22ef1a5e3acc111f09291adde2e76e05fae53
Kris1027 <obarzanek.work@gmail.com>
2024-10-31 20:55:21 +0100

    format document

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,23 +1,26 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     image: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-}, {
-    timestamps: true // createdAt, updatedAt
-});
+  },
+  {
+    timestamps: true, // createdAt, updatedAt
+  }
+);
 
-const Product = mongoose.model('Product', productSchema)
+const Product = mongoose.model('Product', productSchema);
 //products
 
 export default Product;
